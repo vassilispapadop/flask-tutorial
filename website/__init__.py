64886216cc394dd046005b4f22422f62c51c7965
Vassilis <vassilispapadop@gmail.com>
2021-03-21 20:52:39 +0200

    db model

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     #name of the file initialize flask
     app = Flask(__name__)
     # encrypt session data, cookies etc, in production dont share secrete key
     app.config['SECRET_KEY'] = '123456789' 
-     
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}' 
+    db.init_app(app)
+    
     # register views/routes
     from .views import views
     from .auth import auth
