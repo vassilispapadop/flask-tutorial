6f40dc5bedc48aff070cbf2cffc00d39ce93271c
Vassilis <vassilispapadop@gmail.com>
2021-03-21 20:33:41 +0200

    flash messaging

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,9 @@
 # routes of website besides auth page
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
@@ -11,6 +11,24 @@ def login():
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        fistName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters', category='error')
+        elif len(fistName) <2 :
+            flash('first name must be greater than 1 characters', category='error')
+        elif password1 != password2:
+            flash('passwords don\'t match', category='error')
+        elif len(password1) < 7:
+            flash('Password must be greater than 4 characters', category='error')
+        else:
+            # add user to database
+            flash('Account created!', category = 'success')
+
     return render_template("sign_up.html")
